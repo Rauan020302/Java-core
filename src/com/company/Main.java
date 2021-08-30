c7ede9751da85533519c056c81108a579cfffd3f
Рауан <ibragimov020302@gmail.com>
2021-08-30 18:44:30 +0600

    loop

---
+++ src/com/company/Main.java
@@ -0,0 +1,61 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	for (int i = 0; i < 4; i++){
+//	    for (int j = 0; j < 5; j++){
+//            System.out.println(i+j);
+//        }
+//    }
+
+//        for (int i = 0; i < 4; i++){
+//            for (int j = 0; j < i; j++){
+//                System.out.println(i*j);
+//            }
+//        }
+
+//        for (int i = 0; i < 4; i++) {
+//            int s = 0;
+//            for (int j = 0; j < i; j++) {
+//                s = s + i + j;
+//                System.out.println(s);
+//            }
+//        }
+
+//        int n = 9;
+//        for (int i = 1; i <= n; i++){
+//            for (int j = 1; j <=n; j++){
+//                System.out.println(i + " * " + j + " = " + i*j + " ");
+//
+//            }
+//            System.out.println();
+//        }
+
+//        for (int i = 10; i < 100; i++){
+//            if (i % 2 == 0){
+//                System.out.print(i + " ");
+//            }
+//            if (i % 3 == 0){
+//                System.out.print(i + " ");
+//            }
+//            if (i % 4 == 0){
+//                System.out.print(i + " ");
+//            }
+//            System.out.println();
+//        }
+
+//        int n=75;
+//
+//        for(int i=1; i<=n; i++) {
+//            System.out.print(i + ": ");
+//            for (int j = 1; j <= i / 2; j++) {
+//                if (i % j == 0)
+//                    System.out.print(j + " ");
+//                System.out.println();
+//
+//            }
+//        }
+
+}
+}
