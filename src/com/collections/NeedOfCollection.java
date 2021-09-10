89727d4929ec8ebb56822f169c8e6e0bd51d80c5
Рауан <ibragimov020302@gmail.com>
2021-09-10 18:48:24 +0600

    Collection inceptions

---
+++ src/com/collections/NeedOfCollection.java
@@ -0,0 +1,35 @@
+package com.collections;
+
+public class NeedOfCollection {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = 30;
+        int e = 30;
+        int f = 30;
+        int g = 30;
+        int h = 30;
+
+        int[] arArray = new int[10000];
+
+        Student[] students = new Student[1000];
+        students[0] = new Student();
+        students[1] = new Student();
+        students[2] = new Student();
+        //students[3] = new Book(); don't
+
+        Book[] books = new Book[200];
+
+        Object[] objects = new Object[20];
+        objects[0] = new Book();
+
+
+    }
+}
+class Book{
+
+}
+class Student{
+
+}
