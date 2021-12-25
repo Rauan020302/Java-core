de8ce9d89153c4b2c3d76ebeb1f89afc93fd1411
Рауан <ibragimov020302@gmail.com>
2021-12-25 12:54:35 +0300

    FractionalKnapsack

--- src/algorithms/Greedy.java
+++ src/algorithms/Greedy.java
@@ -8,7 +8,36 @@ public class Greedy {
     public static void main(String[] args) {
         int[] digits = {3,1,7,9,9,5};
         System.out.println(maxNumberFromDigits(digits));
+
+        System.out.println();
+
+        int[] stations = {0,200,375,550,750,950};
+
+        System.out.println(minStops(stations,400));
+
     }
+    public static int minStops(int[] stations, int capacity) {
+        int result = 0;
+        int currentStop = 0;
+
+        while (currentStop < stations.length - 1) {
+            int nextStop = currentStop;
+
+            while (nextStop < stations.length -1 &&
+                    stations[nextStop + 1] - stations[currentStop] <= capacity) {
+                nextStop++;
+
+            }
+            if (currentStop == nextStop)
+                return -1;
+            if (nextStop < stations.length - 1) {
+                result++;
+            }
+            currentStop = nextStop;
+        }
+        return result;
+    }
+
 
     public static String maxNumberFromDigits(int[] digits) {
         return String.join("",Arrays.stream(digits).boxed()
