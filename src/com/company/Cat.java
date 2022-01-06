c531202875db67110cf68dbadff3b46e0d99bfb4
Рауан <ibragimov020302@gmail.com>
2022-01-06 17:11:17 +0300

    PrintWriter and PrintReader

---
+++ src/com/company/Cat.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
+public class Cat {
+    public String name;
+    public int age;
+    public int weight;
+
+    public void save(PrintWriter writer) throws Exception {
+        writer.println(name);
+        writer.println(age);
+        writer.println(weight);
+        writer.flush();
+    }
+    public void load(BufferedReader reader) throws Exception {
+        name = reader.readLine();
+        age = Integer.parseInt(reader.readLine());
+        weight = Integer.parseInt(reader.readLine());
+    }
+}
