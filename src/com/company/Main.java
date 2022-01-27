1556b54c0fee1122c80ed8cb2624f8626cb3f176
Рауан <ibragimov020302@gmail.com>
2022-01-27 13:44:52 +0300

    Suleimanov StreamApi lesson

---
+++ src/com/company/Main.java
@@ -0,0 +1,59 @@
+package com.company;
+
+import com.company.model.Specialist;
+import com.company.model.Specialty;
+import com.company.util.StreamApiUtil;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //List<Specialist> specialists = OldApproachUtil.getSpecialists();
+        List<Specialist> specialists = StreamApiUtil.getSpecialists();
+
+        // Filter
+//        List<Specialist> engineers = OldApproachUtil.filterBySpecialty(specialists, Specialty.ENGINEER);
+        List<Specialist> engineers = StreamApiUtil.filterBySpecialty(specialists, Specialty.ENGINEER);
+        StreamApiUtil.printSpecialists(engineers);
+
+        // Sorting
+        List<Specialist> sortedSpecialistsAsc = StreamApiUtil.sortSpecialistsByNameAsc(specialists);
+        List<Specialist> sortedSpecialistsDesc = StreamApiUtil.sortSpecialistsByNameDesc(specialists);
+        StreamApiUtil.printSpecialists(sortedSpecialistsAsc);
+        StreamApiUtil.printSpecialists(sortedSpecialistsDesc);
+
+        // Max salary
+        Specialist withMaxSalary = StreamApiUtil.findWithMaxSalary(specialists);
+        System.out.println(withMaxSalary);
+        // Min salary
+        Specialist withMinSalary = StreamApiUtil.findWithMinSalary(specialists);
+        System.out.println(withMinSalary);
+
+        // Grouping
+        Map<Specialty, List<Specialist>> groupedBySpecialty = StreamApiUtil.groupBySpecialty(specialists);
+        System.out.println(groupedBySpecialty);
+
+        // All engineers
+//        boolean allEngineers = OldApproachUtil.matchAllEngineers(specialists);
+        boolean allEngineers = StreamApiUtil.matchAllEngineers(specialists);
+        System.out.println(allEngineers);
+
+        // Any engineers
+//        boolean anyEngineer = OldApproachUtil.matchAnyEnginner(specialists);
+        boolean anyEngineer = StreamApiUtil.matchAnyEngineer(specialists);
+        System.out.println(anyEngineer);
+
+        // All salary more then
+        //boolean allSalaryMoreThen1000 = OldApproachUtil.matchAllSalaryMoreThen(specialists, new BigDecimal(10000));
+        boolean allSalaryMoreThen1000 = StreamApiUtil.matchAllSalaryMoreThen(specialists, new BigDecimal(10000));
+        System.out.println(allSalaryMoreThen1000);
+
+        // No one with salary more then
+//        boolean noOneWithSalaryMoreThen10000 = OldApproachUtil.matchNoneSalaryMoreThen(specialists, new BigDecimal(10000));
+        boolean noOneWithSalaryMoreThen10000 = StreamApiUtil.matchNoneSalaryMoreThen(specialists, new BigDecimal(10000));
+        System.out.println(noOneWithSalaryMoreThen10000);
+    }
+}
